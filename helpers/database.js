e2cf33bc6a0c5cc22e4c2224d01462fcc40847a7
Joe Standring <standringj@coventry.ac.uk>
2020-10-07 14:34:36 +0000

    Fix styling

--- helpers/database.js
+++ helpers/database.js
@@ -1,20 +1,18 @@
 // Interface to MySQL
 
-const mySQL = require('promise-mysql');
-const info = require('../config');
+const mysql = require('promise-mysql');
+const info = require('../.config.js');
 
 // Called by models. Converts data to valid MySQL query
 exports.run_query = async function runQuery(query, values) {
   try {
     // Run the query in MySQL
     const connection = await mysql.createConnection(info.config);
-    let data = await connection.query(query, values);
+    const data = await connection.query(query, values);
     await connection.end();
-    
     return data;
-  } catch(error) {
+  } catch (error) {
     // Display a generic error for security
-    console.error(error, query, values);
-    throw 'Database query error';
+    throw new Error('Database query error');
   }
-}
+};
