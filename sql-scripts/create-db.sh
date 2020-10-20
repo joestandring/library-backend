8e9ba524d174f24882766e7ca224ae09d6e2e19d
Joe Standring <standringj@coventry.ac.uk>
2020-10-20 13:44:04 +0000

    Create add-roles script

--- sql-scripts/create-db.sh
+++ sql-scripts/create-db.sh
@@ -6,10 +6,13 @@ PASSWORD="$2"
 
 # Create DB and add tables
 mysql -u "$USERNAME" -p"$PASSWORD" -e "CREATE DATABASE books"
-mysql -u "$USERNAME" -p"$PASSWORD" books < ./create-roles.sql
-mysql -u "$USERNAME" -p"$PASSWORD" books < ./create-users.sql
-mysql -u "$USERNAME" -p"$PASSWORD" books < ./create-books.sql
-mysql -u "$USERNAME" -p"$PASSWORD" books < ./create-requests.sql
+mysql -u "$USERNAME" -p"$PASSWORD" books < sql-scripts/create-roles.sql
+mysql -u "$USERNAME" -p"$PASSWORD" books < sql-scripts/create-users.sql
+mysql -u "$USERNAME" -p"$PASSWORD" books < sql-scripts/create-books.sql
+mysql -u "$USERNAME" -p"$PASSWORD" books < sql-scripts/create-requests.sql
+
+# Add roles
+mysql -u "$USERNAME" -p"$PASSWORD" books < sql-scripts/add-roles.sql
 
 # Show DB
 mysql -u "$USERNAME" -p"$PASSWORD" books -e "SHOW TABLES"
