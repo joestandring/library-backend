ca8c50d5275da0d3ebc2cffc55fad98a08536407
Joe Standring <standringj@coventry.ac.uk>
2020-12-01 10:14:30 +0000

    Fix getByBookID route

--- routes/requests.js
+++ routes/requests.js
@@ -196,7 +196,7 @@ async function remove(ctx) {
 router.get('/', auth, getAll);
 router.get('/:id([0-9]{1,})', auth, getByID);
 router.get('/user/:id([0-9]{1,})', auth, getByUserID);
-router.get('/:id([0-9]{1,})', auth, getByBookID);
+router.get('/book/:id([0-9]{1,})', auth, getByBookID);
 router.post('/', auth, bodyParser(), validateRequest, create);
 router.put('/:id([0-9]{1,})', auth, bodyParser(), validateRequest, update);
 router.del('/:id([0-9]{1,})', auth, remove);
