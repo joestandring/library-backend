6a491efebe37e608e63a76e8ba407c432d5ff7b5
Joe Standring <standringj@coventry.ac.uk>
2020-12-02 11:17:50 +0000

    Test environtemnt db

--- __tests__/check_jest_works.js
+++ __tests__/check_jest_works.js
@@ -1,4 +1,7 @@
-// This test fails because 1 !== 2
 it('Testing to see if Jest works', () => {
   expect(1).not.toBe(2)
 });
+
+test('Jest should use the correct database', () => {
+  expect(process.env.DB_DATABASE).toBe('test');
+})
